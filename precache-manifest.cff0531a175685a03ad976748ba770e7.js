591d796e6f0d52f76616560da588a8640b40a495
Karim <cseed322@gmail.com>
2020-11-27 18:39:35 +0300

    Updates

---
+++ precache-manifest.cff0531a175685a03ad976748ba770e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70390764df6adf41f5a6692345f5917",
+    "url": "/react-fake-auth/index.html"
+  },
+  {
+    "revision": "cb22b91b25f112377b74",
+    "url": "/react-fake-auth/static/css/2.9c82a2bb.chunk.css"
+  },
+  {
+    "revision": "37274eb616568578b714",
+    "url": "/react-fake-auth/static/css/main.6b395357.chunk.css"
+  },
+  {
+    "revision": "cb22b91b25f112377b74",
+    "url": "/react-fake-auth/static/js/2.99e386dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-fake-auth/static/js/2.99e386dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37274eb616568578b714",
+    "url": "/react-fake-auth/static/js/main.6d88aba9.chunk.js"
+  },
+  {
+    "revision": "34e77e8f2179397f6c50",
+    "url": "/react-fake-auth/static/js/runtime-main.9b90ba23.js"
+  }
+]);
